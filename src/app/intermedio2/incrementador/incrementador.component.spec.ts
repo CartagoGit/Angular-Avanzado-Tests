0f04c2d1d34a68eb0753725408024e6e2373527f
Cartago <cartago.relaxingcup@gmail.com>
2023-06-03 15:49:50 +0200

    Al hacer el componente como standalone en el test debemos importarlo y no declaralo, ya que los standalone se comportan en la practica como los modulos

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -2,26 +2,20 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
 
-
 describe('Incremendator Component', () => {
+  let component: IncrementadorComponent;
+  let fixture: ComponentFixture<IncrementadorComponent>;
 
-    let component: IncrementadorComponent;
-    let fixture: ComponentFixture<IncrementadorComponent>;
-
-    beforeEach( () => {
-        TestBed.configureTestingModule({
-            declarations: [ IncrementadorComponent ],
-            imports: [ FormsModule ]
-        });
-
-        fixture = TestBed.createComponent(IncrementadorComponent);
-        component = fixture.componentInstance;
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // declarations: [ IncrementadorComponent ],
+      // imports: [ FormsModule ]
+      imports: [FormsModule, IncrementadorComponent],
     });
 
-    it('', () => {
+    fixture = TestBed.createComponent(IncrementadorComponent);
+    component = fixture.componentInstance;
+  });
 
-
-    });
-
+  it('', () => {});
 });
