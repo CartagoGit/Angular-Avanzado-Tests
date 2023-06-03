d7696d1d113d0ff8a10f633401d15ccfe0853d89
Cartago <cartago.relaxingcup@gmail.com>
2023-06-03 17:37:28 +0200

    Relizamos test sobre html al realizar cambios en los valores con metodos

--- src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
-describe('Incremendator Component', () => {
+describe('Incremendator Component ->', () => {
   let component: IncrementadorComponent;
   let fixture: ComponentFixture<IncrementadorComponent>;
 
@@ -17,5 +18,26 @@ describe('Incremendator Component', () => {
     component = fixture.componentInstance;
   });
 
-  it('', () => {});
+  it('Debe mostrar la leyenda', () => {
+    component.leyenda = 'Progreso de carga';
+    fixture.detectChanges();
+
+    const element: HTMLElement = fixture.debugElement.query(
+      By.css('h3')
+    ).nativeElement;
+
+    expect(element.innerHTML).toContain('Progreso de carga');
+  });
+
+  it('Debe mostrar en el input el valor del progreso', () => {
+    component.cambiarValor(5);
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      const input = fixture.debugElement.query(By.css('input'));
+      const element: HTMLInputElement = input.nativeElement;
+
+      expect(element.value).toBe('55');
+    });
+  });
 });
