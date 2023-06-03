c599d3ba6cd6b6570a3fcf842eefe8c292d31c01
Cartago <cartago.relaxingcup@gmail.com>
2023-06-03 13:07:54 +0200

    Creamos nuestro primer espia para hacer una llamada falta a un servicio y retornar medicos

--- src/app/intermedias/espias/medicos.component.spec.ts
+++ src/app/intermedias/espias/medicos.component.spec.ts
@@ -1,12 +1,22 @@
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
+import { from } from 'rxjs';
 
 describe('MedicosComponent ->', () => {
   let componente: MedicosComponent;
+  const servicio = new MedicosService(null as any);
 
-  beforeEach(() => {});
+  beforeEach(() => {
+    componente = new MedicosComponent(servicio);
+  });
 
-  it('holi', () => {
-    expect(true).toBeTruthy();
+  it('Init: debe cargar los médicos', () => {
+    const medicos = ['medico1', 'medico2', 'medico3'];
+    spyOn(servicio, 'getMedicos').and.callFake(() => {
+      return from([medicos]);
+    });
+    componente.ngOnInit();
+
+    expect(componente.medicos.length).toBeGreaterThan(0);
   });
 });
