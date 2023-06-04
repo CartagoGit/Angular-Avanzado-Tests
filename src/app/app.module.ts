e81f15453b88ee5a4819ef08e72341e5e42f4135
Cartago <cartago.relaxingcup@gmail.com>
2023-06-04 20:39:25 +0200

    Cargamos el RouterTestingModule en el spec de la app principal y añadimos router outler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppComponent } from './app.component';
 import { MedicosComponent } from 'downloads/Material+de+espías/medicos.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
 import { HospitalComponent } from './intermedio2/hospital/hospital.component';
+import { RouterModule } from '@angular/router';
+import { RUTAS } from './avanzado/rutas/app.routes';
 
 @NgModule({
-  declarations: [AppComponent, MedicosComponent, MedicoComponent, HospitalComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MedicosComponent,
+    MedicoComponent,
+    HospitalComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(RUTAS)],
   providers: [],
   bootstrap: [AppComponent],
 })
