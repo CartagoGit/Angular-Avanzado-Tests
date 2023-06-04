5f90388f579ae334da38691838739a0e395de45a
Cartago <cartago.relaxingcup@gmail.com>
2023-06-04 20:59:04 +0200

    Creamos un test para comprobar que exista el router-outlet

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
-describe('AppComponent', () => {
-  beforeEach(() =>
+fdescribe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [RouterTestingModule.withRoutes([])],
-    })
-  );
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Angular-Avanzado-Tests'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('Angular-Avanzado-Tests');
+  });
+
+  it('Debe de tener un router-outlet', () => {
+    // const compiled = fixture.nativeElement;
+    // expect(compiled.querySelector('router-outlet')).toBeTruthy();
+    const compiled = fixture.debugElement;
+    const routerOutlet = compiled.query(By.directive(RouterOutlet));
+    expect(routerOutlet).toBeTruthy();
+    // expect(routerOutlet).not.toBeNull();
   });
 });
--- src/app/avanzado/rutas/app.routes.spec.ts
+++ src/app/avanzado/rutas/app.routes.spec.ts
@@ -1,7 +1,7 @@
 import { MedicoComponent } from 'src/app/intermedio2/medico/medico.component';
 import { RUTAS } from './app.routes';
 
-describe('Rutas principales ->', () => {
+fdescribe('Rutas principales ->', () => {
   it('Debe existir la ruta /', () => {
     expect(RUTAS).toContain({
       path: 'medico/:id',
